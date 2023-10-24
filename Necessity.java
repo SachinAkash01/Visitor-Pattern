94452d8305afd259ec268d712500297e26d6387f
SachinAkash01 <akashmusix01@gmail.com>
2023-10-24 09:25:22 +0530

    Initial commit

---
+++ Necessity.java
@@ -0,0 +1,15 @@
+class Necessity implements Visitable {
+	private double price;
+
+	Necessity(double item) {
+		price = item;
+	}
+
+	public double accept(Visitor visitor) {
+		return visitor.visit(this);
+	}
+
+	public double getPrice() {
+		return price;
+	}
+}
