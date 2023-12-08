539a0eaff1043f049665d3000de7bc23aabf9cf4
Sachin Akash <akashmusix01@gmail.com>
2023-12-08 17:08:32 +0530

    Remove unnecessary newlines

--- Visitor.java
+++ Visitor.java
@@ -1,6 +1,5 @@
 // The visitor pattern is used when you have to perform
 // the same action on many objects of different types
-
 interface Visitor {
 	// Created to automatically use the right 
 	// code based on the Object sent
