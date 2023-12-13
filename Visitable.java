69f78061152d1a704951997be3c67c7e52339af8
Sachin Akash <akashmusix01@gmail.com>
2023-12-12 16:15:29 -1200

    Remove unnecessary newlines

--- Visitable.java
+++ Visitable.java
@@ -3,7 +3,6 @@ interface Visitable {
 	// Allows the Visitor to pass the object so
 	// the right operations occur on the right 
 	// type of object.
-	
 	// accept() is passed the same visitor object
 	// but then the method visit() is called using 
 	// the visitor object. The right version of visit()
